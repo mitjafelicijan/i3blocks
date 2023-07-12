fff50e49d700f3c527913c6a4d7021e25282a552
Mitja Felicijan <m@mitjafelicijan.com>
2023-07-12 15:52:56 +0200

    Rewrite of ram to c

--- ram.c
+++ ram.c
@@ -8,5 +8,10 @@ int main(int argc, char *argv[]) {
   int percentage = get_arg_value("-p", argc, argv, 0);
   int width = get_arg_value("-w", argc, argv, 10);
 
+  int available_mb = get_cmd_value("free --mega | grep 'Mem' | awk '{print $2}'");
+  int used_mb = get_cmd_value("free --mega | grep 'Mem' | awk '{print $3}'");
+  int used = (used_mb * 100) / (available_mb + used_mb);
+
+  display_progress_bar(used, width, percentage);
   exit(EXIT_SUCCESS);
 }
